862630f6a6267b80ecb505848ac6a77255edf7c8
Parimala01 <parimala.palle@tigeranalytics.com>
2022-11-13 13:59:03 +0530

    terraform modules

---
+++ modules/functionapp/variables.tf
@@ -0,0 +1,24 @@
+variable storage_name {
+  type        = string
+  description = "Name of the Storage Account"
+}
+
+variable location {
+  type        = string
+  description = "Location of resource group"
+}
+
+variable resource_group_name {
+  type        = string
+  description = "Name of the Resource Group"
+}
+
+variable service_plan {
+  type        = string
+  description = "name of service plan"
+}
+
+variable functionapp {
+  type        = string
+  description = "name of function app"
+}
